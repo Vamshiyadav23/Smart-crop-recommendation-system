ce22303a0f1a5982653b73e11d8c44844e5e1ca2
Vamshiyadav23 <vamshiyadav23@hotmail.com>
2026-02-19 22:43:27 +0530

    completed

---
+++ src/models/train_model.py
@@ -0,0 +1,42 @@
+import pandas as pd
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load dataset
+df = pd.read_csv("data/Crop_recommendation.csv")
+
+# Separate features and target
+X = df.drop("label", axis=1)
+y = df["label"]
+
+# Train-test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Initialize model
+model = RandomForestClassifier(
+    n_estimators=100,
+    random_state=42
+)
+
+# Train model
+model.fit(X_train, y_train)
+
+# Predictions
+y_pred = model.predict(X_test)
+
+# Evaluation
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy:.2f}")
+
+print("\nClassification Report:")
+print(classification_report(y_test, y_pred))
+
+
+joblib.dump(model, "../models/crop_model.pkl")
+
+print("\nModel saved successfully!")
